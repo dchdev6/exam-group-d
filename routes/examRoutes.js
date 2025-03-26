d0f93cce441032bd46fdf1100ab2308b9ba9fe97
unknown <TT405@ccs.local>
2025-03-26 02:36:31 -0700

    Added functionality: Exam Updates

--- routes/examRoutes.js
+++ routes/examRoutes.js
@@ -5,4 +5,24 @@ router.get('/', (req, res) => {
     res.json({ message: "Group D API" });
 });
 
+router.put("/exams/:id", (req, res) => {
+    const examId = parseInt(req.params.id);
+    const updatedExam = req.body;
+    let examFound = false;
+
+    exams = exams.map(exam => {
+        if (exam.id === examId) {
+          examFound = true;
+          return { ...exam, ...updatedExam };
+        }
+        return exam;
+      });
+
+    if (!examFound) {
+        return res.status(404).json({ message: "Exam not found" });
+      }
+    
+      res.json({ message: "Exam updated successfully" });
+
+    });
 module.exports = router;
